38e6bd555ed10d7b84f0da533b7a9ab9a0332b2c
Code Sandbox <hello@codesandbox.io>
2023-06-07 13:09:15 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ab393501c244a8185bac59debc9807b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9cd4d0153c2c89b905e",
+    "url": "https://.github.io/csb-e7l9rn/static/css/main.9f325a58.chunk.css"
+  },
+  {
+    "revision": "c9cd4d0153c2c89b905e",
+    "url": "https://.github.io/csb-e7l9rn/static/js/main.c9be8f4c.chunk.js"
+  },
+  {
+    "revision": "edf1955ace5a37936a3c",
+    "url": "https://.github.io/csb-e7l9rn/static/js/runtime~main.88510f6d.js"
+  },
+  {
+    "revision": "39b9fc788c4137a2435c",
+    "url": "https://.github.io/csb-e7l9rn/static/css/2.fe0bf043.chunk.css"
+  },
+  {
+    "revision": "39b9fc788c4137a2435c",
+    "url": "https://.github.io/csb-e7l9rn/static/js/2.bc8d1592.chunk.js"
+  },
+  {
+    "revision": "d45db513e5e009570190b2294648d6d3",
+    "url": "https://.github.io/csb-e7l9rn/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://.github.io/csb-e7l9rn/precache-manifest.ab393501c244a8185bac59debc9807b3.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://.github.io/csb-e7l9rn/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
